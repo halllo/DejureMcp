1fb2f99a9cfc2a6891ac1d2d0afb3733666d4099
Manuel Naujoks <mnaujoks@gmail.com>
2025-11-02 19:57:23 +0100

    rechtsgebiete and gesetze as resources

--- DejureMcp.Stdio/Program.cs
+++ DejureMcp.Stdio/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Dejure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
+using System.Text.Json;
 
 var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
@@ -11,7 +13,10 @@ builder.Services.AddHttpClient<DejureOrgHttpClient>(httpClient =>
 	httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0");
 });
 
-builder.Services.AddMcpServer().WithStdioServerTransport().WithToolsFromAssembly();
+builder.Services.AddMcpServer()
+	.WithStdioServerTransport()
+	.WithToolsFromAssembly()
+	.WithResourcesFromAssembly();
 
 await builder.Build().RunAsync();
 
@@ -20,22 +25,22 @@ public class DejureTools(DejureOrgHttpClient dejureOrgHttpClient)
 {
 	[McpServerTool(Name = "dejure_rechtsgebiete_auflisten", Title = "Rechtsgebiete auflisten", Destructive = false, Idempotent = true, OpenWorld = false, ReadOnly = true)]
 	[Description("Listet alle Rechtsgebiete und deren Gesetze auf.")]
-	public async Task<List<Rechtsgebiet>> GetRechtsgebiete()
+	public async Task<List<RechtsgebietDto>> GetRechtsgebiete()
 	{
 		var dejurOrg = await dejureOrgHttpClient.Load();
 		var rechtsgebiete = dejurOrg.Rechtsgebiete
-			.Select(r => new Rechtsgebiet(r.Name, [.. r.Gesetze.Select(g => new Gesetz(g.Kürzel, g.Name))]))
+			.Select(r => new RechtsgebietDto(r.Name, [.. r.Gesetze.Select(g => new GesetzDto(g.Kürzel, g.Name))]))
 			.ToList();
 		return rechtsgebiete;
 	}
 
 	[McpServerTool(Name = "dejure_gesetze_auflisten", Title = "Gesetze auflisten", Destructive = false, Idempotent = true, OpenWorld = false, ReadOnly = true)]
 	[Description("Listet alle Gesetze auf.")]
-	public async Task<List<Gesetz>> GetGesetze()
+	public async Task<List<GesetzDto>> GetGesetze()
 	{
 		var dejurOrg = await dejureOrgHttpClient.Load();
 		var gesetze = dejurOrg.Gesetze
-			.Select(g => new Gesetz(g.Kürzel, g.Bezeichnung))
+			.Select(g => new GesetzDto(g.Kürzel, g.Bezeichnung))
 			.ToList();
 		return gesetze;
 	}
@@ -48,7 +53,7 @@ public class DejureTools(DejureOrgHttpClient dejureOrgHttpClient)
 		var gesetz = dejurOrg.Gesetze.Single(g => string.Equals(g.Kürzel, gesetzesKuerzel, StringComparison.InvariantCultureIgnoreCase));
 		var inhaltsverzeichnnis = await gesetz.LoadInhaltsverzeichnis();
 		var paragraphs = inhaltsverzeichnnis.Paragraphen
-			.Select(p => new Paragraph(p.Nummer, p.Name))
+			.Select(p => new ParagraphDto(p.Nummer, p.Name))
 			.ToList();
 		return new GetParagraphsResponse(inhaltsverzeichnnis.Intro, paragraphs);
 	}
@@ -72,10 +77,10 @@ public class DejureTools(DejureOrgHttpClient dejureOrgHttpClient)
 			Rechtsprechungen: [.. suchergebnis.Rechtsprechungen.Select(r => new SuchenResponse.Rechtsprechung(r.Urteil, r.Detail))]);
 	}
 
-	public record Rechtsgebiet(string Name, List<Gesetz> Gesetze);
-	public record Gesetz(string Kuerzel, string Name);
-	public record GetParagraphsResponse(string Intro, List<Paragraph> Paragraphs);
-	public record Paragraph(string Nummer, string Name);
+	public record RechtsgebietDto(string Rechtsgebiet, List<GesetzDto> Gesetze);
+	public record GesetzDto(string Kuerzel, string Gesetz);
+	public record GetParagraphsResponse(string Intro, List<ParagraphDto> Paragraphs);
+	public record ParagraphDto(string Nummer, string Paragraph);
 	public record ReadParagraphResponse(string Intro, string Text);
 	public record SuchenResponse(List<SuchenResponse.Gesetz> Gesetze, List<SuchenResponse.Gesetzgebung> Gesetzgebungen, List<SuchenResponse.Rechtsprechung> Rechtsprechungen)
 	{
@@ -83,4 +88,79 @@ public class DejureTools(DejureOrgHttpClient dejureOrgHttpClient)
 		public record Gesetzgebung(string Gesetz, string Detail);
 		public record Rechtsprechung(string Urteil, string Detail);
 	}
+}
+
+[McpServerResourceType]
+public class DejureResources(DejureOrgHttpClient dejureOrgHttpClient)
+{
+	[McpServerResource(UriTemplate = "dejure://rechtsgebiete", Name = "Rechtsgebiete", MimeType = "application/json")]
+	[Description("Rechtsgebiete")]
+	public async Task<string> RechtsgebieteResource()
+	{
+		var dejurOrg = await dejureOrgHttpClient.Load();
+		return JsonSerializer.Serialize(dejurOrg.Rechtsgebiete.Select(r => r.Name));
+	}
+
+	[McpServerResource(UriTemplate = "dejure://rechtsgebiete/{rechtsgebiet}", Name = "Rechtsgebiete", MimeType = "application/json")]
+	[Description("Rechtsgebiete")]
+	public async Task<TextResourceContents> RechtsgebieteResource(RequestContext<ReadResourceRequestParams> requestContext, string rechtsgebiet)
+	{
+		var dejurOrg = await dejureOrgHttpClient.Load();
+		var dto = dejurOrg.Rechtsgebiete.FirstOrDefault(r => string.Equals(r.Name, rechtsgebiet, StringComparison.InvariantCultureIgnoreCase));
+		if (dto != null)
+		{
+			return new TextResourceContents
+			{
+				Text = JsonSerializer.Serialize(new
+				{
+					Rechtsgebiet = dto.Name,
+					Gesetze = dto.Gesetze.Select(g => new GesetzDto(g.Kürzel, g.Name)).ToList(),
+				}),
+				MimeType = "application/json",
+				Uri = requestContext.Params!.Uri
+			};
+		}
+		else
+		{
+			throw new NotSupportedException($"Unknown resource: {requestContext.Params?.Uri}");
+		}
+	}
+
+	[McpServerResource(UriTemplate = "dejure://gesetze", Name = "Gesetze", MimeType = "application/json")]
+	[Description("Rechtsgebiete")]
+	public async Task<string> GesetzeResource()
+	{
+		var dejurOrg = await dejureOrgHttpClient.Load();
+		return JsonSerializer.Serialize(dejurOrg.Gesetze.Select(g => new GesetzDto(g.Kürzel, g.Name)));
+	}
+
+	[McpServerResource(UriTemplate = "dejure://gesetze/{kuerzel}", Name = "Gesetze", MimeType = "application/json")]
+	[Description("Gesetze")]
+	public async Task<TextResourceContents> GesetzeResource(RequestContext<ReadResourceRequestParams> requestContext, string kuerzel)
+	{
+		var dejurOrg = await dejureOrgHttpClient.Load();
+		var gesetz = dejurOrg.Gesetze.FirstOrDefault(g => string.Equals(g.Kürzel, kuerzel, StringComparison.InvariantCultureIgnoreCase));
+		if (gesetz != null)
+		{
+			var inhaltsverzeichnis = await gesetz.LoadInhaltsverzeichnis();
+			InhaltsverzeichnnisDto dto = new InhaltsverzeichnnisDto(
+				inhaltsverzeichnis.Intro,
+				[.. inhaltsverzeichnis.Paragraphen.Select(p => new ParagraphDto(p.Nummer, p.Name))]
+			);
+			return new TextResourceContents
+			{
+				Text = JsonSerializer.Serialize(dto),
+				MimeType = "application/json",
+				Uri = requestContext.Params!.Uri
+			};
+		}
+		else
+		{
+			throw new NotSupportedException($"Unknown resource: {requestContext.Params?.Uri}");
+		}
+	}
+
+	public record GesetzDto(string Kuerzel, string Gesetz);
+	public record InhaltsverzeichnnisDto(string Intro, List<ParagraphDto> Paragraphen);
+	public record ParagraphDto(string Kuerzel, string Paragraph);
 }
